6ee52a3e2d0c9493483d24a052ad2644507f1aed
Razaul007 <raselharuyamare@gmail.com>
2024-03-15 03:07:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+       <Header></Header>
+       <div className='md: flex'>
+          <Blogs></Blogs>
+          <Bookmarks></Bookmarks>
+       </div>
+  
+    </>
+  )
+}
+
+export default App
